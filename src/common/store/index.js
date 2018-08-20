12158fa73acca7c3710661b8c62334f4de1e606c
吕玉龙 <lvyl@hrtps.com>
2018-08-20 13:38:03 +0800

    vuex  store 中方法优化

--- src/common/store/index.js
+++ src/common/store/index.js
@@ -1,13 +1,27 @@
+
 const storeConfig = {
-    state: {
-        local: {}
+    state:{
+        local:{}
     },
-    getters: {},
-    mutations: {
-        setProp(state, data) {
-            state[data.key] = data.val;
+    getters:{
+
+    },
+    mutations:{
+        setProp(state,data){
+            // data支持数组和对象两种类型参数
+            if (Array.isArray(data)){
+                data.map(function (v,k) {
+                    state[v.key] = v.val;
+
+                })
+            }else{
+                state[data.key] = data.val;
+            }
+
         }
     },
-    actions: {}
+    actions:{
+
+    }
 };
 export default storeConfig;
