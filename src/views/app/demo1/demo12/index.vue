db940f4717cd3842c07b6ebddca93dde2d25bf93
吕玉龙 <lvyl@hrtps.com>
2018-12-21 12:10:51 +0800

    使用 lv-common-css组件

--- src/views/app/demo1/demo12/index.vue
+++ src/views/app/demo1/demo12/index.vue
@@ -2,7 +2,10 @@
     <div class="page">
         <page-header title="demo12列表"></page-header>
         <div class="page-content">
-            demo12列表 此页面只是用来给侧边栏凑数的，实际内容看demo11
+            <div class="red bd-b primary">
+                demo12列表 此页面只是用来给侧边栏凑数的，实际内容看demo11
+            </div>
+
         </div>
     </div>
 </template>
